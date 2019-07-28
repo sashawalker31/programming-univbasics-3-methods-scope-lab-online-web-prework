ebf5c935f0e844ea42837ef6e4a88173eee66b24
sasha walker <sashawalker31@gmail.com>
2019-07-28 13:08:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,19 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
- 
+ phrase= "It's-a me, Mario!"
 end
 
 def toadstool
   status= 'Thank You Mario! But Our Princess Is In Another Castle'
   puts status
 end
+
+def link
+  puts phrase
+  phrase="It's Dangerous To Go Alone! Take This"
+end
+
+def any_phrase
+  puts phrase
+  phrase="Do A Barrel Roll!"
